a8b57a6834666d43fb82c75cbe790b3132cd8de0
Iyad Benabbes <46727943+iyadfox@users.noreply.github.com>
2023-03-10 13:03:26 +0100

    Update Yalidineweb.py

--- Yalidineweb.py
+++ Yalidineweb.py
@@ -1,10 +1,6 @@
 import time
-import requests
-from bs4 import BeautifulSoup
-import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pandas as pd
 
 
 driver = webdriver.Firefox()
